3fb37244b4bf36c0e326dfde171d2fc8e60c4dc3
Lokor4ik <lokor4ik@gmail.com>
2020-08-27 17:58:32 +0300

    added get & delete Post Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -10,36 +10,42 @@ const Post = require('../../models/Post');
 // @route  POST api/posts
 // @desc   Create a post
 // @access Private
-router.post('/',
-  [
-    auth,
-    [
-      check('text', 'Text is required').not().isEmpty(),
-    ]
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+router.post('/', auth, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    try {
-      const user = await User.findById(req.user.id).select('-password');
+  try {
+    const user = await User.findById(req.user.id).select('-password');
 
-      const newPost = new Post({
-        text: req.body.text,
-        name: user.name,
-        avatar: user.avatar,
-        user: req.user.id
-      });
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    });
 
-      const post = await newPost.save();
+    const post = await newPost.save();
 
-      res.json(post);
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send('Server Error');
-    }
-  });
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  GET api/posts
+// @desc   Get all posts
+// @access Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
