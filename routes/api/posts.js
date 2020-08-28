8b7ba58358febed44fd564e808f402293aa9c4c6
Lokor4ik <lokor4ik@gmail.com>
2020-08-28 09:26:24 +0300

    completely added get & delete post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -51,12 +51,52 @@ router.get('/', auth, async (req, res) => {
 // @route  GET api/posts/:id
 // @desc   Get post by ID
 // @access Private
-router.get('/', auth, async (req, res) => {
+router.get('/:id', auth, async (req, res) => {
   try {
-    const posts = await Post.findById(req.params.id).sort({ date: -1 });
+    const posts = await Post.findById(req.params.id);
+
+    if (!posts) {
+      return res.status(404).json({ msg: 'Post nor found' });
+    }
+
     res.json(posts);
   } catch (error) {
     console.error(error.message);
+
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post nor found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  DELETE api/posts/:id
+// @desc   Delete a post
+// @access Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post nor found' });
+    }
+
+    // Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (error) {
+    console.error(error.message);
+
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post nor found' });
+    }
+
     res.status(500).send('Server Error');
   }
 });
