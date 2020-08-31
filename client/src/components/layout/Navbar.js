1304781d1b23f9297f200645bb9c196c8065b16e
Lokor4ik <lokor4ik@gmail.com>
2020-08-31 15:37:29 +0300

    added initial components, landing and navbar

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,22 @@
+﻿import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <a href="/">
+          <i class="fas fa-code"></i>
+          DevUnite
+        </a>
+      </h1>
+
+      <ul>
+        <li><a href="/">Developers</a></li>
+        <li><a href="/">Register</a></li>
+        <li><a href="/">Login</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
