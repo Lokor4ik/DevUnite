3d5c6e88999284b21d312dbec256bc5e46cb676c
Lokor4ik <lokor4ik@gmail.com>
2020-09-02 09:18:02 +0300

    added login form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 ﻿import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -7,18 +8,39 @@ const Register = () => {
     password: '',
     passwordDouble: ''
   });
-
+  /* 
+  const newUser = {
+          name,
+          email,
+          password
+        };
+  
+        try {
+          const config = {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          };
+  
+          const body = JSON.stringify(newUser);
+  
+          const res = await axios.post('/api/users', body, config);
+          console.log(res.data);
+        } catch (error) {
+          console.error(error.response.data);
+        }
+  */
   const { name, email, password, passwordDouble } = formData;
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
     if (password !== passwordDouble) {
       console.log('Passwords do not match');
     } else {
-      console.log(formData);
+      console.log('SUCCESS');
     }
   }
 
@@ -73,7 +95,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </section>
   );
