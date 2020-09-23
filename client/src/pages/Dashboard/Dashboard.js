00e6dd25819820e7cd8a4c72422d40290d844225
Lokor4ik <lokor4ik@gmail.com>
2020-09-23 09:49:32 +0300

    continuing create dashboard component

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,18 +1,20 @@
-﻿import React from 'react';
+﻿import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCurrentProfile } from 'store/profile/action';
-import { useEffect } from 'react';
 import Loader from 'components/commons/Loader/Loader';
+import { UserOutlined } from '@ant-design/icons';
 
 const Dashboard = () => {
   const dispatch = useDispatch();
   const { profile, loading } = useSelector(state => state.profileUser);
+  const { user } = useSelector(state => state.auth);
 
   useEffect(() => {
     dispatch(getCurrentProfile());
   }, [dispatch]);
 
-  if (loading || !profile) {
+  if (loading) {
     return <Loader />
   }
 
@@ -20,7 +22,23 @@ const Dashboard = () => {
     <div className="pages-wrapper">
       <section className="dashboard">
         <div className="container">
-          fdsfs
+          <h1 className="large text-primary">Dashboard</h1>
+          <p className="lead">
+            <UserOutlined className='user-outlined' />
+            Welcome {user && user.name}
+          </p>
+          {
+            profile
+              ? `has profile`
+              : (
+                <>
+                  <p>You haven't yet setup a profile, please add some info.</p>
+                  <Link to='/create-profile' className='btn btn-primary my-1'>
+                    Create profile
+                  </Link>
+                </>
+              )
+          }
         </div>
       </section>
     </div>
