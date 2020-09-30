fe1c78f54bfcce57ceb3c724efe71af320fd8529
Lokor4ik <lokor4ik@gmail.com>
2020-09-30 15:39:25 +0300

    added NotFound page

---
+++ client/src/pages/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="pages-wrapper">
+      <section className="profile">
+        <div className="container">
+          NOT FOUND
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default NotFound;
