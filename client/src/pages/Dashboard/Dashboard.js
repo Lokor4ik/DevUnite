3a383d7787de88b997aac011d275dff9ccc6b5c6
Lokor4ik <lokor4ik@gmail.com>
2020-10-01 17:04:51 +0300

    deleted action load profile

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,19 +1,14 @@
-﻿import React, { useEffect } from 'react';
+﻿import React from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { getCurrentProfile } from 'store/profile/action';
+import { useSelector } from 'react-redux';
 import Loader from 'components/commons/Loader/Loader';
 import MainLayout from 'hoc/MainLayout';
+import DashboardActions from 'components/Dashboard/DashboardActions';
 import { UserOutlined } from '@ant-design/icons';
 
 const Dashboard = () => {
-  const dispatch = useDispatch();
-  const { profile, loading } = useSelector(state => state.profileUser);
+  const { hasProfile, loading } = useSelector(state => state.profileUser);
   const { user } = useSelector(state => state.auth);
-
-  useEffect(() => {
-    dispatch(getCurrentProfile());
-  }, [dispatch]);
 
   if (loading) {
     return <Loader />
@@ -27,14 +22,14 @@ const Dashboard = () => {
           Welcome {user && user.name}
       </p>
       {
-        profile
-          ? `has profile`
+        hasProfile
+          ? <DashboardActions />
           : (
             <>
               <p>You haven't yet setup a profile, please add some info.</p>
-              <Link to='/create-profile' className='btn btn-primary my-1'>
+              <Link to='/profile' className='btn btn-primary my-1'>
                 Create profile
-                  </Link>
+              </Link>
             </>
           )
       }
