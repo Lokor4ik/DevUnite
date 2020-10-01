0b456cc4720c43732245c2cd57348f36929b3eee
Lokor4ik <lokor4ik@gmail.com>
2020-10-01 17:09:53 +0300

    changed fields

--- client/src/store/profile/reducer.js
+++ client/src/store/profile/reducer.js
@@ -3,10 +3,31 @@
   GET_PROFILE_SUCCESS,
   GET_PROFILE_ERROR,
   CLEAR_PROFILE,
+  CREATE_PROFILE_REQUEST,
+  CREATE_PROFILE_SUCCESS,
+  CREATE_PROFILE_ERROR,
 } from './types';
 
 const initialState = {
-  profile: null,
+  profile: {
+    company: '',
+    website: '',
+    location: '',
+    status: '',
+    skills: [],
+    bio: '',
+    githubUserName: '',
+    experience: [],
+    education: [],
+    social: {
+      youTube: '',
+      twitter: '',
+      facebook: '',
+      linkedIn: '',
+      instagram: '',
+    },
+  },
+  hasProfile: false,
   profiles: [],
   repos: [],
   loading: true,
@@ -15,17 +36,24 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case CREATE_PROFILE_REQUEST:
     case GET_PROFILE_REQUEST:
       return {
         ...state,
         loading: true,
       }
+    case CREATE_PROFILE_SUCCESS:
     case GET_PROFILE_SUCCESS:
       return {
         ...state,
-        profile: action.payload,
+        profile: {
+          ...state.profile,
+          ...action.payload,
+        },
+        hasProfile: true,
         loading: false,
       }
+    case CREATE_PROFILE_ERROR:
     case GET_PROFILE_ERROR:
       return {
         ...state,
@@ -35,7 +63,25 @@ const reducer = (state = initialState, action) => {
     case CLEAR_PROFILE:
       return {
         ...state,
-        profile: null,
+        profile: {
+          company: '',
+          website: '',
+          location: '',
+          status: '',
+          skills: [],
+          bio: '',
+          githubUserName: '',
+          experience: [],
+          education: [],
+          social: {
+            youTube: '',
+            twitter: '',
+            facebook: '',
+            linkedIn: '',
+            instagram: '',
+          },
+        },
+        hasProfile: false,
         profiles: [],
         repos: [],
         loading: false,
